088aa113f02af5f5742749aa4cd9824237f82dac
Aluno <aluno@etec.sp.gov.br>
2022-10-19 22:49:18 -0300

    exercicio 1 arvore binaria

--- aula 1/src/index.js
+++ aula 1/src/index.js
@@ -1,26 +1,34 @@
-var n1 = 82;
-var n2 = 27;
-var op = "potencia";
+function CalculaH(ca, co){
+    return Math.sqrt(Math.pow(ca, 2) + Math.pow(co, 2));
+}
+function CalculaCa(co, h){
+    return Math.sqrt(Math.pow(h, 2) - Math.pow(co, 2));
+}
+function CalculaCo(ca, h){
+    return Math.sqrt(Math.pow(h, 2) - Math.pow(ca, 2));
+}
 
-function somaNumeros() {
+function CalculaPitagoras(h, ca, co){
 
-    if (op == "+") {
-        return (n1 + n1);
-    }
-    if (op == "-") {
-        return (n1 - n2);
-    }
-    if (op == "*") {
-        return (n1 * n2);
-    }
-    if (op == "/") {
-        return (n1 / n2);
-    }
-    if (op == "raiz") {
-        return Math.sqrt((n1 + n2));
-    }
-    if (op == "potencia") {
-        return Math.pow((n1, n2))
+if(h == 0 ){
+    return CalculaH(ca, co);
+}else if(ca == 0){
+    return CalculaCa(co, h);
+}else{
+    return CalculaCo(ca,h);
     }
 }
-console.log(somaNumeros(5, 4, "potencia"));
+
+function recebePitagoras(){
+    let h = document.getElementById("h").value;
+    let ca = document.getElementById("ca").value;
+    let co = document.getElementById("co").value;
+
+    console.log("Resultado: "+CalculaPitagoras(h, ca, co));
+    document.getElementById("resposta").innerHTML=CalculaPitagoras(h, ca, co);
+}
+/*
+console.log("Teste 1: "+CalculaPitagoras(0,21,20));
+console.log("Teste 2: "+CalculaPitagoras(13,5,0));
+console.log("Teste 3: "+CalculaPitagoras(10,0,6));
+*/
